b5d1e190cc864ed6f4e6c83ffc89802f674ecc15
IRABARUTA <willynorbert53@gmail.com>
2025-04-18 23:22:38 +0200

    doing readme

--- src/components/CtaSection.tsx
+++ src/components/CtaSection.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button';
 
 const CtaSection: React.FC = () => {
   return (
-    <section className="bg-blue-600 text-white md:py-12 py-5">
+    <section className="bg-blue-800 text-white md:py-12 py-5">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row container-sm  justify-center p-4 md:gap-20 gap-6 items-center">
           <h2 className=" md:text-4xl text-3xl font-bold  mb-3 md:mb-0 md:text-start  text-center">
