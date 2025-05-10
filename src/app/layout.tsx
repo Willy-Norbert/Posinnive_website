6de18a420ec05ef6180ce89c430e2e856a07ccdc
IRABARUTA <willynorbert53@gmail.com>
2025-05-10 15:21:25 +0200

    solve issue with google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata, Viewport } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
+import { Geist, Geist_Mono } from 'next/font/google'
+
 import "./globals.css";
 
 // Load custom Google fonts
